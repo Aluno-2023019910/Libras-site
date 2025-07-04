c368470e8e2c659a78f202cce1efb5ac4df29a49
Alexsander <2023019910@aluno.osorio.ifrs.edu.br>
2025-07-04 18:16:47 -0300

    quase

--- LibrasParaTodos/forum-libra/src/app/page.jsx
+++ LibrasParaTodos/forum-libra/src/app/page.jsx
@@ -12,29 +12,19 @@ export default function Home() {
   }
 
 
-  if (session) {
+ 
      return (
      
-      <div>
-         <button onClick={() => signOut()}>Logout</button>
-         <p>Logado como: {session.user?.email}</p>
-        <p>Nome: {session.nome}</p>
-        <p>Admin: {session.adm ? "Sim" : "Não"}</p>
-        
+        <div className="min-h-screen bg-gradient-to-br from-blue-100 via-white to-blue-200 flex items-center justify-center">
+          <div className="max-w-xl text-center p-8 bg-white shadow-2xl rounded-2xl">
+            <h1 className="text-4xl font-bold text-blue-700 mb-4">Bem-vindo ao Libras para Todos</h1>
+            <p className="text-gray-700 text-lg mb-6">
+              Projeto para a materia de 
+            </p>
+           </div>
         </div>
      )     
-  }
+  
 
-  return (
-   
-      
-      <div>
-        
-      
-        <Login></Login>
-        
-        
-      </div>
-     
-  );
+  
 }
